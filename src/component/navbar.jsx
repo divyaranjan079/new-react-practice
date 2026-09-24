2ab327cbcd7440ac29bf12644a7eb92b9d22f0c2
divyaranjan079 <divyaranjan079@gmail.com>
2026-09-24 13:05:32 +0530

    react pratice2

---
+++ src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className='nav'>
+
+        <h1>hii iam divya.</h1>
+
+    </div>
+
+  )
+}
+
+export default navbar
---
+++ src/component/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function test() {
+  return (
+    <div className="cards">
+        <h1>hii iam divya.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, eveniet.</p>
+    </div>
+  )
+}
+
+export default test
